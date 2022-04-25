2a1f1cd9ff3763ebce12184cb35a8a85f59eb792
memarino92 <memarino92@gmail.com>
2022-04-24 22:18:26 -0400

    feat: added homepage

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,10 @@ describe('Renders the homepage successfully', () => {
   it('can navigate to the home page', () => {
     cy.visit('localhost:8910')
   })
+
+  it('contains the word "Home"', () => {
+    cy.visit('localhost:8910')
+
+    cy.contains('Home')
+  })
 })
